d7cc4361845893a4d1c61d4c349fe7f84ab4f24b
Muhamad Alfin Pratama <muhamadalfinpratamaa@gmail.com>
2024-02-08 23:59:49 +0700

    fix method search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -20,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathName}?${params.toString()}`)
 
 
-  }, 100)
+  }, 500)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
