ece401182094c131fe799f22d81baa06f36a21c9
Muhamad Alfin Pratama <muhamadalfinpratamaa@gmail.com>
2024-02-08 21:44:50 +0700

    implement Card Skeleton in dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,38 +1,28 @@
 import React from 'react'
 import { lusitana } from '../../ui/fonts'
-import {
-    Card
-} from '../../ui/dashboard/cards'
+import CardWrapper from '../../ui/dashboard/cards'
 import LatestInvoices from '../../ui/dashboard/latest-invoices'
 import RevenueChart from '../../ui/dashboard/revenue-chart'
 import { Suspense } from 'react'
-import { RevenueChartSkeleton } from '@/app/ui/skeletons'
-
-import { fetchLatestInvoices, fetchCardData } from '../../lib/data'
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons'
 
 
 const Dashboard = async () => {
-
-
-
-    const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData()
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Card title='Collected' value={totalPaidInvoices} type='collected' />
-                <Card title='Pending' value={totalPaidInvoices} type='pending' />
-                <Card title='Total Invoices' value={numberOfInvoices} type='invoices' />
-                <Card title='Total Customers' value={numberOfCustomers} type='customers' />
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
-                <Suspense>
-
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
                     <LatestInvoices />
                 </Suspense>
             </div>
