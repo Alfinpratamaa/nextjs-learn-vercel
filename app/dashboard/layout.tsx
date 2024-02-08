026727a85b6515dfa4a34be02e536b6ec548a4f7
Muhamad Alfin Pratama <muhamadalfinpratamaa@gmail.com>
2024-02-08 23:39:50 +0700

    fix bug in layout dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,6 @@ const layout = ({ children }: { children: React.ReactNode }) => {
         <div className={`${inter.className} flex h-screen flex-col md:flex-row md:overflow-hidden`}>
             <div className="w-full flex-none md:w-64">
                 <SideNav />
-                {children}
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
